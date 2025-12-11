ecdf079421f5daaf81e93fd98f8a91e1723097cf
Martin Johansson <majo5918@student.umu.se>
2025-12-11 11:14:52 +0100

    Renamed main

---
+++ __main__.py
@@ -0,0 +1,26 @@
+"""Module for initialising a `TextAdventureGame`.
+
+Main entry point of the VG assignment program.
+"""
+
+import argparse
+import sys
+
+import gamespeare
+from gamespeare.game import TextAdventureGame
+from gamespeare.playbook import GameDataError
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Gamespeare - A Text Adventure Game Engine",
+    )
+    parser.add_argument("filename", help="path of game to play")
+    args = parser.parse_args()
+
+    game_file = args.filename
+    try:
+        playbook = gamespeare.playbook.from_file(game_file)
+    except GameDataError as e:
+        print(f"Unable to load {game_file}: {e}", file=sys.stderr)
+    else:
+        TextAdventureGame(playbook).play()
